8fec1bdb58d38760dfada9da7a64d860b9831ae9
unknown <myrickbr@clarkson.edu>
2020-05-18 17:00:26 -0400

    Implemented user info class, basic functions

--- include/userinformation.h
+++ include/userinformation.h
@@ -5,7 +5,56 @@
 class userInformation
 {
 public:
+    /* Enumerations */
+    enum class Gender{Null, Male, Female};
+
+    /* Constructors */
     userInformation();
+
+    /* Getters and Setters */
+    const int & get_height_inches();
+    const int & get_height_feet();
+    const int * get_weight_pounds();
+    const Gender * get_gender();
+    const int * get_age_years();
+    const int * get_days_exercise_per_week();
+    const double * get_bmi();
+    const int * get_bmi_percentile();
+    const double * get_calorie_intake();
+    void set_height_inches(const int &);
+    void set_height_feet(const int &);
+    void set_weight_pounds(const int *);
+    void set_gender(const Gender *);
+    void set_age_years(const int *);
+    void set_days_exercise_per_week(const int *);
+    void set_bmi(const double *);
+    void set_bmi_percentile(const int *);
+    void set_calorie_intake(const double *);
+
+
+
+
+    /* Other Functions */
+    double calculate_BMI();
+    double calculate_BMI_percentile();
+
+
+
+
+private:
+
+    int height_inches;
+    int height_feet;
+    int weight_pounds;
+    Gender gender;
+    int age_years;
+    int days_exericise_per_week;
+    double bmi;
+    int bmi_percentile;
+    double calorie_intake;
+
 };
+
+
 
 #endif // USERINFORMATION_H
--- src/userinformation.cpp
+++ src/userinformation.cpp
@@ -1,6 +1,84 @@
-#include "userinformation.h"
+#include "../include/userinformation.h"
+#include <iostream>
 
 userInformation::userInformation()
 {
+    /* Initialize values to null (except height values since default is 5' 7") */
 
+    this->height_inches = 7;
+    this->height_feet = 5;
+    this->weight_pounds = NULL;
+    this->age_years = NULL;
+    this->gender = Gender::Null;
+    this->age_years = NULL;
+    this->days_exericise_per_week = NULL;
+    this->bmi = NULL;
+    this->bmi_percentile = NULL;
+
+}
+
+const int & userInformation::get_height_inches(){
+    return this->height_inches;
+}
+const int & userInformation::get_height_feet(){
+    return this->height_feet;
+}
+const int * userInformation::get_weight_pounds(){
+    return &(this->weight_pounds);
+}
+const userInformation::Gender * userInformation::get_gender(){
+    return &(this->gender);
+}
+const int * userInformation::get_age_years(){
+    return &(this->age_years);
+}
+const int * userInformation::get_days_exercise_per_week(){
+    return &(this->days_exericise_per_week);
+}
+const double * userInformation::get_bmi(){
+    return &(this->bmi);
+}
+const int * userInformation::get_bmi_percentile(){
+    return &(this->bmi_percentile);
+}
+const double * userInformation::get_calorie_intake(){
+    return &(this->calorie_intake);
+}
+void userInformation::set_height_inches(const int & heightInches){
+    this->height_inches = heightInches;
+}
+void userInformation::set_height_feet(const int & heightFeet){
+    this->height_feet = heightFeet;
+}
+void userInformation::set_weight_pounds(const int * weightPounds){
+    this->weight_pounds = *weightPounds;
+}
+void userInformation::set_gender(const userInformation::Gender * gender_){
+    this->gender = * gender_;
+}
+void userInformation::set_age_years(const int * ageYears){
+    this->age_years = *ageYears;
+}
+void userInformation::set_days_exercise_per_week(const int * daysExercisePerWeek){
+    this->days_exericise_per_week = * daysExercisePerWeek;
+}
+void userInformation::set_bmi(const double * bmi_){
+    this->bmi = *bmi_;
+}
+void userInformation::set_bmi_percentile(const int * bmiPercentile){
+    this->bmi_percentile = *bmiPercentile;
+}
+void userInformation::set_calorie_intake(const double * calorieIntake){
+    this->calorie_intake = *calorieIntake;
+}
+
+
+double userInformation::calculate_BMI(){
+
+    return 0.0;
+}
+
+double userInformation::calculate_BMI_percentile(){
+
+    return 0.0;
 }
